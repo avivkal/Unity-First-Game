ee8527263595371979a5f8cf9726feaa33d3f72f
avivkal <avivkalmanson@gmail.com>
2018-01-03 22:23:42 +0200

    Roll A Ball
    Roll a ball and try to collect the items

---
+++ Roll A Ball Real/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position; //ofset = difference between camera and player
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset; // set the camera position to the player position plus the starting point
+    }
+}
